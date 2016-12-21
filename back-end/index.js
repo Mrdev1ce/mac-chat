06be27fe7603e7251aeb2479e05e9068bc3665ec
JS Maksim <JS Maksim>
2016-12-22 00:15:41 +0400

    Added support of https on server

--- back-end/index.js
+++ back-end/index.js
@@ -1,15 +1,26 @@
+let fs = require('fs');
+let http = require('http');
+let https = require('https');
 let express = require('express');
 let bodyParser = require('body-parser');
 let socketIO = require('socket.io');
 let app = express();
-let server = require('http').createServer(app);
-let socket = socketIO(server);
+
+let privateKey = fs.readFileSync('../../server.key', 'utf8');
+let certificate = fs.readFileSync('../../server.pem', 'utf8');
+let credentials = {key: privateKey, cert: certificate, passphrase: 'device', rejectUnauthorized: false, requestCert: true};
+
+let httpServer = http.createServer(app);
+let httpsServer = https.createServer(credentials, app);
+
+let socket = socketIO(httpsServer);
 
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
-server.listen(3000);
+httpServer.listen(3000);
+httpsServer.listen(3443);
 
 app.post('/api/SignInAsUser', function(req, res) {
   console.log('SignInAsUser > ', req.body);
